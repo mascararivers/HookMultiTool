e8a16850ccf867aa726717220b227c750825ce5d
mascara <mascara@rivers.com>
2025-03-16 23:38:58 +0000

    added GUI with iced.rs

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,147 @@
 use anyhow::{Ok, Result};
+use iced::widget::checkbox;
+use iced::Length::Fill;
+use iced::widget::column;
+use iced::widget::container;
+use iced::widget::{button, text, text_input};
+use iced::{Element, Task};
 use serde_json::json;
-use tokio::io::{self, AsyncBufReadExt};
 
-async fn request(message: &str, avatar_url: &str, username: &str) -> Result<()> {
+#[derive(Debug, Clone)]
+enum Message {
+    Send,
+    Response,
+    ChangeHookUrl(String),
+    ChangeMessage(String),
+    ChangeAvatarUrl(String),
+    ChangeUsername(String),
+    HasEmbed(bool)
+}
 
-    let embed = json!([
-        {
-            "title": "a",
-            "type": "rich",
-            "description": "aa"
-        }
-    ]);
-    
-    let payload = json!({
-        "content": message,
-        "avatar_url": avatar_url,
-        "username": username,
-        "embeds": embed
+#[derive(Default)]
+struct Hook {
+    hook_url: String,
+    message: String,
+    avatar_url: Option<String>,
+    username: Option<String>,
+    has_embed: bool,
+    embed: Option<Embed>,
+}
+
+#[derive(serde::Serialize, Default, Clone)]
+struct Field {
+    name: String,
+    value: String,
+    inline: bool
+}
+#[derive(serde::Serialize, Default, Clone)]
+struct Embed {
+    title: String,
+    description: String,
+    fields: Vec<Field>,
+}
+
+async fn request(message: String, avatar_url: String, username: String, hook_url: String,
+embed: Option<Embed>) -> Result<()> {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async move {
+        let e = if embed.is_none() {
+            json!([])
+        } 
+        else {
+            let embed_some = embed.unwrap();
+            json!([
+                {
+                    "title": embed_some.title,
+                    "type": "rich",
+                    "description": embed_some.description,
+                    "fields": embed_some.fields
+                }
+            ])
+        };
+
+        let payload = json!({
+            "content": message,
+            "avatar_url": avatar_url,
+            "username": username,
+            "embeds": e
+        });
+
+        let client = reqwest::Client::new();
+        let res = client.post(hook_url).json(&payload).send().await.unwrap();
+        println!("{:?}", res.text().await);
     });
 
-    let client = reqwest::Client::new();
-    let res = client.post("https://discord.com/api/webhooks/1350248908445712435/MPGunb1_eGlZQk7G1stwl0aG6bfU6zV3TpOSiLzAJT1thhNvxepNnqMO1ckyC_0OfFq6")
-            .json(&payload)            
-            .send()
-            .await?;
-    println!("{}", res.text().await?);
     return Ok(());
 }
 
-async fn user_input() {
-    let mut avatar_url = String::new();
-    let mut username = String::new();
 
-    let mut customize = String::new();
-    let mut cin = io::BufReader::new(io::stdin());
-    println!("Customize Webhook? y/n");
-    let _ = cin.read_line(&mut customize).await;
+impl Hook {
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::Send => {
+                let message = self.message.clone();
+                let avatar_url = self.avatar_url.clone().unwrap_or_default();
+                let username = self.username.clone().unwrap_or_default();
+                let hook_url = self.hook_url.clone();
+                let embed = if !self.has_embed {
+                    None
+                } else {
+                    Some(Embed::default())
+                };
 
-    match customize.trim() {
-        "y" | "Y" => {
-            println!("avatar url for webhook");
-            let mut stdin = io::BufReader::new(io::stdin());
-            let _ = stdin.read_line(&mut avatar_url).await;
+                let embed_title = embed.clone().unwrap().title;
 
-            println!("username for webhook");
-            let mut uin = io::BufReader::new(io::stdin());
-            let _ = uin.read_line(&mut username).await;
-
-            let _ = request("test", &avatar_url.as_str(), &username).await;
+                return Task::perform(request(message, avatar_url, username, hook_url, embed), |_| {
+                    Message::Response
+                });
+            }
+            Message::Response => {
+                return Task::none();
+            }
+            Message::ChangeHookUrl(hook) => self.hook_url = hook,
+            Message::ChangeAvatarUrl(url) => self.avatar_url = Some(url), 
+            Message::ChangeMessage(msg) => self.message = msg,
+            Message::ChangeUsername(usr) => self.username = Some(usr),
+            Message::HasEmbed(has) => self.has_embed = has
         }
-        "n" | "N" => {
-            let _ = request("test", "", "").await;
-        }
-        _ => {
-            println!("Invalid input. Please enter 'y' or 'n'.");
-        }
+        Task::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        let btn = button(text("Send")).on_press(Message::Send);
+
+        let embed_checkbox = checkbox("Has Embed", self.has_embed)
+        .on_toggle(Message::HasEmbed);
+
+        let clmn = column![
+            text_input("Webhook URL", &self.hook_url).on_input(Message::ChangeHookUrl),
+            text_input("Message", &self.message).on_input(Message::ChangeMessage),
+            text_input("Avatar URL", self.avatar_url.as_deref().unwrap_or("")).on_input(Message::ChangeAvatarUrl),
+            text_input("Username", self.username.as_deref().unwrap_or("")).on_input(Message::ChangeUsername),
+            embed_checkbox,
+            btn
+        ];
+        container(clmn)
+            .width(iced::Length::Fill)
+            .height(iced::Length::Fill)
+            .center(Fill)
+            .into()
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    user_input().await;
-    Ok(())
+async fn main() -> iced::Result {
+    let theme = |_s: &Hook| iced::Theme::Dark;
+
+    //user_input().await;
+    iced::application("Hook Multitool", Hook::update, Hook::view)
+        .theme(theme)
+        .window_size((400.0, 400.0))
+        .centered()
+        .run()
 }
