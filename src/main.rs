90dc19a74a5b5658c6d8e06e9ee842edc91d76c6
winlogon <walker84837@gmail.com>
2025-03-17 01:37:05 +0100

    chore: sort some imports, and apply formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use anyhow::{Ok, Result};
-use iced::widget::checkbox;
-use iced::Length::Fill;
-use iced::widget::column;
-use iced::widget::container;
-use iced::widget::{button, text, text_input};
-use iced::{Element, Task};
-use serde::de;
+use anyhow::Result;
+use iced::{
+    Element,
+    Length::Fill,
+    Task,
+    widget::{button, checkbox, column, container, text, text_input},
+};
+use log::info;
 use serde_json::json;
-use log::{info, warn, error};
 
 #[derive(Debug, Clone)]
 enum Message {
@@ -20,7 +19,7 @@ enum Message {
     HasEmbed(bool),
 
     ChangeEmbedTitle(String),
-    ChangeEmbedDescription(String)
+    ChangeEmbedDescription(String),
 }
 
 #[derive(Default)]
@@ -37,7 +36,7 @@ struct Hook {
 struct Field {
     name: String,
     value: String,
-    inline: bool
+    inline: bool,
 }
 #[derive(serde::Serialize, Default, Clone, Debug)]
 struct Embed {
@@ -46,9 +45,13 @@ struct Embed {
     fields: Vec<Field>,
 }
 
-async fn request(message: String, avatar_url: String, username: String, hook_url: String,
-embed: Option<Embed>) -> Result<()> {
-
+async fn request(
+    message: String,
+    avatar_url: String,
+    username: String,
+    hook_url: String,
+    embed: Option<Embed>,
+) -> Result<()> {
     info!("{:?}", &embed.clone().unwrap());
 
     let rt = tokio::runtime::Builder::new_current_thread()
@@ -59,8 +62,7 @@ embed: Option<Embed>) -> Result<()> {
     rt.block_on(async move {
         let e = if embed.is_none() {
             json!([])
-        } 
-        else {
+        } else {
             let embed_some = embed.unwrap();
             json!([
                 {
@@ -90,7 +92,6 @@ embed: Option<Embed>) -> Result<()> {
     return Ok(());
 }
 
-
 impl Hook {
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
@@ -101,23 +102,22 @@ impl Hook {
                 let hook_url = self.hook_url.clone();
                 let embed = self.embed.clone();
 
-                return Task::perform(request(message, avatar_url, username, hook_url, embed), |_| {
-                    Message::Response
-                });
+                return Task::perform(
+                    request(message, avatar_url, username, hook_url, embed),
+                    |_| Message::Response,
+                );
             }
             Message::Response => {
                 return Task::none();
             }
             Message::ChangeHookUrl(hook) => self.hook_url = hook,
-            Message::ChangeAvatarUrl(url) => self.avatar_url = Some(url), 
+            Message::ChangeAvatarUrl(url) => self.avatar_url = Some(url),
             Message::ChangeMessage(msg) => self.message = msg,
             Message::ChangeUsername(usr) => self.username = Some(usr),
             Message::HasEmbed(has) => {
                 self.has_embed = has;
-                self.embed = if has {
-                    Some(Embed::default())
-                } else { None }
-            },
+                self.embed = if has { Some(Embed::default()) } else { None }
+            }
 
             Message::ChangeEmbedTitle(title) => {
                 if self.embed.is_none() {
@@ -126,13 +126,13 @@ impl Hook {
                 if let Some(embed) = &mut self.embed {
                     embed.title = title;
                 }
-            },
+            }
             Message::ChangeEmbedDescription(description) => {
                 if self.embed.is_none() {
                     return Task::none();
                 }
 
-                if let Some(embed) = &mut  self.embed {
+                if let Some(embed) = &mut self.embed {
                     embed.description = description;
                 }
             }
@@ -143,17 +143,34 @@ impl Hook {
     fn view(&self) -> Element<Message> {
         let btn = button(text("Send")).on_press(Message::Send);
 
-        let embed_checkbox = checkbox("Has Embed", self.has_embed)
-        .on_toggle(Message::HasEmbed);
+        let embed_checkbox = checkbox("Has Embed", self.has_embed).on_toggle(Message::HasEmbed);
 
         let clmn = column![
             text_input("Webhook URL", &self.hook_url).on_input(Message::ChangeHookUrl),
             text_input("Message", &self.message).on_input(Message::ChangeMessage),
-            text_input("Avatar URL", self.avatar_url.as_deref().unwrap_or("")).on_input(Message::ChangeAvatarUrl),
-            text_input("Username", self.username.as_deref().unwrap_or("")).on_input(Message::ChangeUsername),
+            text_input("Avatar URL", self.avatar_url.as_deref().unwrap_or(""))
+                .on_input(Message::ChangeAvatarUrl),
+            text_input("Username", self.username.as_deref().unwrap_or(""))
+                .on_input(Message::ChangeUsername),
             embed_checkbox,
-            text_input("Embed Title", self.embed.as_ref().unwrap_or(&Embed::default()).title.as_str()).on_input(Message::ChangeEmbedTitle),
-            text_input("Embed Description", self.embed.as_ref().unwrap_or(&Embed::default()).description.as_str()).on_input(Message::ChangeEmbedDescription),
+            text_input(
+                "Embed Title",
+                self.embed
+                    .as_ref()
+                    .unwrap_or(&Embed::default())
+                    .title
+                    .as_str()
+            )
+            .on_input(Message::ChangeEmbedTitle),
+            text_input(
+                "Embed Description",
+                self.embed
+                    .as_ref()
+                    .unwrap_or(&Embed::default())
+                    .description
+                    .as_str()
+            )
+            .on_input(Message::ChangeEmbedDescription),
             btn
         ];
         container(clmn)
